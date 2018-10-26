e48af62f00e6f6f88a1366e147eb31e9b45b5114
yashrajb <basanyash627@gmail.com>
2018-10-26 08:47:14 -0700

    added env varibales

--- config/mongoose-connect.js
+++ config/mongoose-connect.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/BlogApp",() => {
+var database_uri = process.env.DATABASE_URI || "mongodb://localhost:27017/BlogApp"
+
+mongoose.connect(database_uri,() => {
 	console.log("mongodb is connected");
 });
 
